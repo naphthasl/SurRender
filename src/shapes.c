4d8d9a3164554ae40b8c880a8b77b72ef2fac880
Myself <numzyx@gmail.com>
2020-08-14 11:44:18 -0400

    tri

--- src/shapes.c
+++ src/shapes.c
@@ -15,13 +15,32 @@ void SR_DrawLine(
 {
 	int dx =  abs (x1 - x0), sx = x0 < x1 ? 1 : -1;
 	int dy = -abs (y1 - y0), sy = y0 < y1 ? 1 : -1; 
-	int err = dx + dy, e2; /* error value e_xy */
+	int err = dx + dy, e2;
 
-	for (;;) {  /* loop */
+	for (;;) {
 		SR_CanvasSetPixel(canvas, x0, y0, colour);
 		if (x0 == x1 && y0 == y1) break;
 		e2 = err << 1;
-		if (e2 >= dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
-		if (e2 <= dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
+		if (e2 >= dy)
+			err += dy;
+			x0 += sx;
+		if (e2 <= dx)
+			err += dx;
+			y0 += sy;
 	}
+}
+
+void SR_DrawTri(
+	SR_Canvas *canvas,
+	SR_RGBAPixel color,
+	int x0,
+	int y0,
+	int x1,
+	int y1,
+	int x2,
+	int y2)
+{
+	SR_DrawLine(canvas, color, x0, y0, x1, y1);
+	SR_DrawLine(canvas, color, x1, y1, x2, y2);
+	SR_DrawLine(canvas, color, x2, y2, x0, y0);
 }
--- src/shapes.h
+++ src/shapes.h
@@ -4,7 +4,7 @@
 	#include "canvas.h"
     #include "colours.h"
     
-	//base line function
+	//line drawing function
     void SR_DrawLine(
 		SR_Canvas *canvas,
 		SR_RGBAPixel color,
@@ -12,4 +12,15 @@
 		int y0,
 		int x1,
 		int y1);
+	
+	//triangle drawing function
+    void SR_DrawTri(
+		SR_Canvas *canvas,
+		SR_RGBAPixel color,
+		int x0,
+		int y0,
+		int x1,
+		int y1,
+		int x2,
+		int y2);
 #endif
